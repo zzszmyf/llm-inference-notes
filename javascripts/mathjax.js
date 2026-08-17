3f7f9e7f4d2d83fe38eb76df4794025262a25f5d
zzszmyf <zzszmyf@outlook.com>
2026-08-17 17:41:48 +0800

    Deploy: math rendering fixes

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -5,10 +5,6 @@ window.MathJax = {
     processEscapes: true,
     processEnvironments: true
   },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  },
   loader: {
     load: ["[tex]/html"]
   }
